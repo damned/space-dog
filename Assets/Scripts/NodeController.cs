f1b8e5c919ca7ff16f38131e17782e6cb0a3adc4
danm <writetodan@yahoo.com>
2016-11-19 20:16:22 +0000

    @damned a bunch of nodes repelling each other (linear)

--- Assets/Scripts/NodeController.cs
+++ Assets/Scripts/NodeController.cs
@@ -1,10 +1,42 @@
 ﻿
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class NodeController : MonoBehaviour {
 
+	private List<Collider> repulsives = new List<Collider>();
 
-	public void FixedUpdate() {
+	public void Start() {
+		Debug.Log ("started");
+	}
+
+	void FixedUpdate()
+	{
+		repulsives.ForEach (repulsive => {
+			float strength = -5;
+			Vector3 repulsiveVector = repulsive.transform.position - transform.position;
+			Rigidbody rd = transform.GetComponent<Rigidbody>();
+			rd.AddForce (repulsiveVector * strength, ForceMode.Force);
+		});
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		Debug.Log ("entered! " + other.tag);
+		if (other.tag == "Node") {
+			if (!repulsives.Contains (other)) {
+				repulsives.Add (other);
+			}
+		}
+	}
+	void OnTriggerExit(Collider other) 
+	{
+		Debug.Log ("exited! " + other.tag);
+		if (other.tag == "Node") {
+			if (repulsives.Contains(other)) {
+				repulsives.Remove(other);
+			}
+		}
 	}
 }
