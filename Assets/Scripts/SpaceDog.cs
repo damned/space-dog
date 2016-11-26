c98251cb0fd20ab695ab46223f8ed4e5c61094e7
danm <writetodan@yahoo.com>
2016-11-26 19:56:21 +0000

    @damned Create thing programatically

---
+++ Assets/Scripts/SpaceDog.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceDog : MonoBehaviour {
+
+	public GameObject nodePrefab;
+
+	// Use this for initialization
+	void Start () {
+		var node = Instantiate(nodePrefab) as GameObject;
+		node.GetComponent<Renderer>().material.color = new Color(1f, 0.5f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
