9368e4acdc58fe25ef649c536199c4d5b7371ec4
danm <writetodan@yahoo.com>
2016-12-03 15:26:20 +0000

    @damned added editor-driven integration tests
    plus sample code driven, not started with that yet - would
    be better but know that getting objects properly initialised
    is much harder don't want to get dragged into that yet.
    
    showed that these integration tests essentially validate
    NodeController repulsions

---
+++ Assets/Tests/IntegrationTests/ThingReplusionDynamicTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[IntegrationTest.DynamicTestAttribute("IntegrationTests")]
+[IntegrationTest.SucceedWithAssertions]
+[IntegrationTest.TimeoutAttribute(1)]
+[IntegrationTest.ExpectExceptions(false, typeof(ArgumentException))]
+[IntegrationTest.ExcludePlatformAttribute()]
+public class ThingRepulsionDynamicTest : MonoBehaviour
+{
+	public void Start()
+	{
+		IntegrationTest.Pass(gameObject);
+	}
+}
